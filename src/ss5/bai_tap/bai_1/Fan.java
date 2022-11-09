80bb7ea7b40f3ee961b1f9f40b1d2e73bd863f6f
thenguyen123 <the78901234@gmail.com>
2022-11-09 16:04:40 +0700

    add

---
+++ src/ss5/bai_tap/bai_1/Fan.java
@@ -0,0 +1,73 @@
+package ss5.bai_tap.bai_1;
+
+public class Fan {
+    public static final int SLOW=1;
+    public static final int MEDIUM=2;
+    public static final int FAST=3;
+    int speed=SLOW;
+    boolean on=false;
+    double radius=5.0;
+    String color="blue";
+    public Fan(){
+
+    }
+
+    public Fan(int speed, boolean on, double radius, String color) {
+        this.speed = speed;
+        this.on = on;
+        this.radius = radius;
+        this.color = color;
+    }
+
+    public boolean isOn() {
+        return this.on;
+    }
+
+    public void setOn(boolean on) {
+        this.on = on;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+    public String toString() {
+        if (this.isOn()) {
+            return "Fan{" +
+                    "speed=" + this.getSpeed() +
+                    ", radius=" + this.getRadius() +
+                    ", color= " + this.getColor()+  " fan is on "+'\'' +
+                    '}';
+        }else {
+            return "Fan{" +
+                    "speed=" + this.getSpeed() +
+                    ", radius= " + this.getRadius() +
+                    ", color= " + this.getColor() +  " fan is off "+'\'' +
+                    '}';
+        }
+
+    }
+
+
+
+}
