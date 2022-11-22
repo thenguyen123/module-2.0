c261ba605ede4bd90f988334a21a61578478419f
thenguyen123 <the78901234@gmail.com>
2022-11-22 17:18:23 +0700

    add

--- src/ss13/bai_tap/ListOfProducts.java
+++ src/ss13/bai_tap/ListOfProducts.java
@@ -117,16 +117,6 @@ public class ListOfProducts {
 
     }
 
-//    @Override
-//    public int compare(ListOfProducts o1, ListOfProducts o2) {
-//        if (o1.getPrices() - o2.getPrices() > 0) {
-//            return 1;
-//        } else if (o1.getPrices() - o2.getPrices() == 0) {
-//            return 0;
-//
-//        } else {
-//            return -1;
-//        }
-//    }
+
 }
 
